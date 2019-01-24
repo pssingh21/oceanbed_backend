df79a5315c3b56eb1f916e213dee8884cdcd479a
Peeyush Singh <peeyushmansingh@gmail.com>
2019-01-24 19:44:02 +0545

    Delete reported post fully functional

--- routes/reportedPosts.js
+++ routes/reportedPosts.js
@@ -4,6 +4,7 @@ var QuotesModel = require('./../models/quotesModel');
 
 module.exports=function(){
 
+	//show all reported quotes
 	router.get('/',function(req,res,next){
 		QuotesModel.find({report:true},function(err,quotes){
 			if(err){
@@ -11,6 +12,16 @@ module.exports=function(){
 			}
 			res.json(quotes);
 		})
+	});
+
+	//delete particular quote
+	router.delete('/delete/:id',function(req,res,next){
+		QuotesModel.findByIdAndRemove(req.params.id,function(err,quotes){
+			if(err){
+				return next(err);
+			}
+			res.json(quotes);
+		});
 	});
 
 	return router;
