424177edf7aff53caec502463cbb76ecd054fca0
pssingh21 <peeyushmansingh@gmail.com>
2020-09-05 22:48:21 +0200

    Migrated database to MongoDB Atlas

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
-module.exports={
-	host:"127.0.0.1",
-	port:"4040",
-	dbUrl:"mongodb://admin:pssingh21@ds237955.mlab.com:37955/oceanbed",
-	jwtSecret:"it_is_secret"
-}
+module.exports = {
+  host: "127.0.0.1",
+  port: "4040",
+  dbUrl:
+    "mongodb+srv://admin:pssingh21@oceanbed.8tihj.mongodb.net/oceanbed?retryWrites=true&w=majority",
+  jwtSecret: "it_is_secret",
+};
